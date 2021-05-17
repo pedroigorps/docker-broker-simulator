76fccfdcfc246b8bac1c5d2ea7f518d98205cda7
Michel Sales <michelsb@gmail.com>
2021-05-17 20:17:36 -0300

    Fix bug: case structure

--- init.sh
+++ init.sh
@@ -1,15 +1,19 @@
-#!/bin/sh
+#!/bin/ash
+set -e
 
-source /docker-entrypoint.sh
+# Set permissions
+user="$(id -u)"
+if [ "$user" = '0' ]; then
+	[ -d "/mosquitto" ] && chown -R mosquitto:mosquitto /mosquitto || true
+fi
+
 nohup /usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf &
 cd /MQTTGEN
-a=$1
-print "$a"
 if [ -z "$1" ]
 then
-      python3 mqttgen.py cenarios/config.json;;
+      python3 mqttgen.py cenarios/config.json
 else
-   case $1 in
+   case "$1" in
       1) python3 mqttgen.py cenarios/cenario1.json;;
       2) python3 mqttgen.py cenarios/cenario2.json;;
       3) python3 mqttgen.py cenarios/cenario3.json;;
@@ -20,7 +24,6 @@ else
       8) python3 mqttgen.py cenarios/cenario8.json;;
       9) python3 mqttgen.py cenarios/cenario9.json;;
       10) python3 mqttgen.py cenarios/cenario10.json;;
-      *) echo "Opcao Invalida!" ;;
+      *) echo "Opcao Invalida!";;
    esac
-#python3 mqttgen.py cenarios/"$1"
 fi
